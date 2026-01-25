37d8efeae464c803fe3fa29e8db62a82d44666a4
Sudeep Chandra B S <122792474+sudeepchandragowda@users.noreply.github.com>
2026-01-26 04:09:19 +0530

    Added naive approach to find power

---
+++ src/Recursion/PowerNaive.java
@@ -0,0 +1,17 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class PowerNaive {
+    static long power(int a, int n) {
+        if (n == 0) return 1;
+        return a*power(a, n-1);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int n = sc.nextInt();
+
+        System.out.println(power(a, n));
+    }
+}
